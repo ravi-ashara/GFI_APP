b1718985ade37c7c7494987948c2ff69b09b4652
ravi-ashara <raviashar94@gmail.com>
2019-12-14 00:02:06 +0530

    Design login page.

--- src/app/Pages/login/login.page.ts
+++ src/app/Pages/login/login.page.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
+  @ViewChild('checkstaySingIng', { static: false }) checkstaySingIng;
+  public loginForm: FormGroup;
+  constructor(public formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.email],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 
-  constructor() { }
+  ionViewWillEnter() {
+    if (localStorage.isRemember === 'true') {
+      const userDetails = JSON.parse(localStorage.getItem('loginDetails'));
+      this.loginForm.patchValue(userDetails);
+      this.checkstaySingIng.nativeElement.checked = true;
+    }
+  }
+
+  submitForm(val: any) {
+    if (this.checkstaySingIng.nativeElement.checked === true) {
+      localStorage.isRemember = 'true';
+    } else {
+      localStorage.isRemember = 'false';
+    }
+    localStorage.loginDetails = JSON.stringify(val.value);
+  }
 }

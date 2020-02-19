eb580629b8b88fbf276e11cceecf3cd3254fd88b
ravi-ashara <raviashar94@gmail.com>
2020-02-20 00:12:40 +0530

    Add app exit functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { ApiCallService } from './Services/api-call/api-call.service';
+import { Component, ViewChild } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, IonRouterOutlet } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  @ViewChild(IonRouterOutlet, { static: false }) routerOutlet: IonRouterOutlet;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public router: Router,
+    public alertModule: ApiCallService
   ) {
     this.initializeApp();
   }
@@ -22,6 +27,25 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.handleHardwareBackButton();
+    });
+  }
+
+  handleHardwareBackButton() {
+    this.platform.backButton.subscribe((event: any) => {
+      if (this.routerOutlet && this.routerOutlet.canGoBack()) {
+        this.routerOutlet.pop();
+      } else if (this.router.url === '/login') {
+        navigator['app'].exitApp();
+      } else {
+        if (this.router.url.includes('/tabs')) {
+          this.alertModule.showConfirm('Goisrael App ','Are you sure you want to exit ?', ['Cancel', 'Exit'], (res: any) => {
+            if (res === "Yes") {
+              navigator['app'].exitApp();
+            }
+          });
+        }
+      }
     });
   }
 }

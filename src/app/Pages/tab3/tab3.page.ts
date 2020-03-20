4cedb09e604019806424cc0ef5bb28549609f1c7
raviashara <raviashar94@gmail.com>
2020-03-21 00:41:14 +0530

    add moment js. Show current week dats in segment.

--- src/app/Pages/tab3/tab3.page.ts
+++ src/app/Pages/tab3/tab3.page.ts
@@ -1,19 +1,37 @@
 import { Component } from '@angular/core';
 import { ApiCallService } from '../../Services/api-call/api-call.service';
-
+import * as moment from 'moment';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
+  totalDays: any = [];
+  currentDate: any = '';
   constructor(public apicall: ApiCallService) { }
-
+  
   presentPopover() {
     this.apicall.showPopover().then((val: any) => {
       console.log(val);
     });
   }
+  
+  ionViewWillEnter() {
+    this.totalDays = this.enumerateDaysBetweenDates(moment().startOf('week'), moment().endOf('week'));
+    this.currentDate = moment().format('DD-MMM');
+  }
 
+  enumerateDaysBetweenDates(startDate: any, endDate: any) {
+    let date = []
+    while (moment(startDate) <= moment(endDate)) {
+      date.push(startDate);
+      startDate = moment(startDate).add(1, 'days');
+    }
+    return date;
+  }
+
+  segmentChanged(val: any) {
+    console.log(val.detail.value);
+  }
 }

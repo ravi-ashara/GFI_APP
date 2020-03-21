3613798efe993ef558d0ff9a1cfdef84f6fe09d0
raviashara <raviashar94@gmail.com>
2020-03-21 16:57:46 +0530

    Set data in create company.

--- src/app/Pages/create-profile/create-profile.page.ts
+++ src/app/Pages/create-profile/create-profile.page.ts
@@ -10,15 +10,42 @@ export class CreateProfilePage {
 
   setStep: string = "step1";
   buttonName: string = '';
+  setp1Data: any = {
+    user_type: '',
+    company_name: '',
+    field: '',
+    country: ''
+  }
+  setp2Data: any = {
+    company_representative: '',
+    title: '',
+    logo: '',
+    profile_pic: '',
+    pager_file: '',
+    company_description: '',
+    website: '',
+    no_of_emp: '',
+    mobile: '',
+  }
+
+  setp3Data: any = {
+    stage: '',
+    raising: ''
+  }
   constructor(private navCtrl: NavController) { }
 
   changeStap(val: any, val_buttonName: any) {
+    if (val == "step2" && val_buttonName == "next") {
+      console.log(this.setp1Data);
+    } else if (val == "step3" && val_buttonName == "next") {
+      console.log(this.setp2Data);
+    }
     this.setStep = val;
     this.buttonName = val_buttonName;
   }
 
   registerProfile() {
-    localStorage.isLogin = true;
-    this.navCtrl.navigateRoot(['/home']);
+    let combineObj = { ...this.setp1Data, ...this.setp2Data, ...this.setp3Data }
+    console.log(combineObj);
   }
 }

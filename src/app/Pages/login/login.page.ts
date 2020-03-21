15809ac119b1eab5fbf53534517d730aab0229cd
raviashara <raviashar94@gmail.com>
2020-03-21 14:35:54 +0530

    Call login api call with error message.

--- src/app/Pages/login/login.page.ts
+++ src/app/Pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
+import { ApiCallService } from '../../Services/api-call/api-call.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -10,10 +11,11 @@ export class LoginPage {
   @ViewChild('checkstaySingIng', { static: false }) checkstaySingIng;
   public loginForm: FormGroup;
   constructor(public formBuilder: FormBuilder,
-    private navCtrl: NavController) {
+    private navCtrl: NavController,
+    private apiService: ApiCallService) {
     this.loginForm = this.formBuilder.group({
-      email: ['', Validators.email],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      u_email: ['', Validators.email],
+      u_password: ['', [Validators.required, Validators.minLength(3)]],
     });
   }
 
@@ -32,7 +34,29 @@ export class LoginPage {
       localStorage.isRemember = 'false';
     }
     localStorage.loginDetails = JSON.stringify(val.value);
-    localStorage.isLogin = true;
-    this.navCtrl.navigateRoot(['/home']);
+    try {
+      this.apiService.showLoader();
+      this.apiService.hitAPICall('post', 'login', val.value).subscribe((response: any) => {
+        this.apiService.hideLoader();
+        if (response.status == "failed") {
+          if (response.errors) {
+            this.apiService.showAlert('', response.errors.u_email[0], 'Ok', () => { });
+          } else {
+            this.apiService.showAlert('', 'Invalid Password.', 'Ok', () => { });
+          }
+        } else {
+          localStorage.isLogin = true;
+          this.navCtrl.navigateRoot(['/home']);
+          localStorage.loginUserData = JSON.stringify(response.data);
+          localStorage.token = response.data.token;
+        }
+      }, (error) => {
+        this.apiService.hideLoader();
+        this.apiService.showAlert('', 'Error form server side.', 'Ok', () => { });
+      });
+    } catch (error) {
+      this.apiService.hideLoader();
+      this.apiService.showAlert('', 'Error form server side.', 'Ok', () => { });
+    }
   }
 }

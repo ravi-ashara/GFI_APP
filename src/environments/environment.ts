f87171660e0b008a88385f278a54cd6a7638b51e
raviashara <raviashar94@gmail.com>
2020-03-21 14:34:28 +0530

    Add baseURL. Add firebaseConfig.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,17 @@
 
 export const environment = {
   production: false,
-  baseURL: ''
+  baseURL: 'http://goformeet.com/v2/goformeet/admin/api/',
+  firebaseConfig: {
+    apiKey: "AIzaSyBq5Ca08Ws3Ov8DUpemXR0EKdX8AeRfFA8",
+    authDomain: "goformeet-60634.firebaseapp.com",
+    databaseURL: "https://goformeet-60634.firebaseio.com",
+    projectId: "goformeet-60634",
+    storageBucket: "goformeet-60634.appspot.com",
+    messagingSenderId: "703559770185",
+    appId: "1:703559770185:web:a1246b77f31739ed8c5f98",
+    measurementId: "G-SCJ9DP9V4Y"
+  }
 };
 
 /*

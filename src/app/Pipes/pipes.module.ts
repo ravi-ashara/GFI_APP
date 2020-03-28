1bab304205337de6f092464cdbeab8e5dfe33203
ravi-ashara <raviashara94@gmail.com>
2020-03-28 16:13:54 +0530

    Add pipes module.

---
+++ src/app/Pipes/pipes.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { SearchPipe } from './search/search.pipe';
+
+@NgModule({
+    declarations: [SearchPipe],
+    imports: [],
+    exports: [SearchPipe]
+})
+export class PipesModule { }

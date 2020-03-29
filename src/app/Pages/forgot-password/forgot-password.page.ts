6cdca39294d910252ba53bc954a1763d88d1115b
ravi-ashara <raviashara94@gmail.com>
2020-03-29 18:10:33 +0530

    Add email field.

--- src/app/Pages/forgot-password/forgot-password.page.ts
+++ src/app/Pages/forgot-password/forgot-password.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.page.html',
   styleUrls: ['./forgot-password.page.scss'],
 })
-export class ForgotPasswordPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ForgotPasswordPage {
+  forgotPasswordForm: any;
+  constructor(public formBuilder: FormBuilder) {
+    this.forgotPasswordForm = this.formBuilder.group({
+      u_email: ['', Validators.email],
+    });
   }
 
+  submitForm(val: any){}
 }

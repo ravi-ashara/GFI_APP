7d9f666baf94d39df8534c7d43862b321dfce3c4
ravi-ashara <raviashara94@gmail.com>
2020-04-01 22:21:25 +0530

    Setup api call.

--- src/app/Pages/sponsor/sponsor.page.ts
+++ src/app/Pages/sponsor/sponsor.page.ts
@@ -9,9 +9,29 @@ import { ApiCallService } from '../../Services/api-call/api-call.service';
 export class SponsorPage {
   currentSponsor: string = "Gold Sponsors";
   totalSponsor = ['Gold Sponsors', 'Silver Sponsors', 'Bronze Sponsors', 'Financial Supporters', 'Governmental Supporters', 'Cooperations', 'Media Coverage']
-  constructor(public apicall: ApiCallService) { }
+  constructor(public apiService: ApiCallService) { }
 
   segmentChanged(val: any) {
     console.log(val.detail.value);
+  }
+
+  ionViewWillEnter() {
+    this.showSponsors();
+  }
+
+  showSponsors() {
+    try {
+      this.apiService.showLoader();
+      this.apiService.hitAPICall('post', 'sponsor/list', '').subscribe((response: any) => {
+        this.apiService.hideLoader();
+        console.log(response);
+      }, error => {
+        this.apiService.hideLoader();
+        this.apiService.showAlert('', 'Error form server side', 'Ok', () => { });
+      });
+    } catch (error) {
+      this.apiService.hideLoader();
+      this.apiService.showAlert('', 'Error form server side', 'Ok', () => { });
+    }
   }
 }

f02fd587acdb2e8db863d5d3b4f7822b75c9587e
ravi-ashara <raviashara94@gmail.com>
2020-04-01 22:18:20 +0530

    Change and add new keys

--- src/app/Pages/book-meeting/book-meeting.page.ts
+++ src/app/Pages/book-meeting/book-meeting.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiCallService } from '../../Services/api-call/api-call.service';
 @Component({
   selector: 'app-book-meeting',
   templateUrl: './book-meeting.page.html',
@@ -12,15 +13,22 @@ export class BookMeetingPage {
   constructor(
     private modalCtrl: ModalController,
     public navParam: NavParams,
-    public formBuilder: FormBuilder) {
+    public formBuilder: FormBuilder,
+    private apiService: ApiCallService) {
     this.modalData = this.navParam.get('value');
     console.log(this.modalData);
     this.bookMeetingForm = this.formBuilder.group({
-      event: ['', Validators.required],
-      date: ['', Validators.required],
+      event_id: ['', Validators.required],
+      meeting_date: ['', Validators.required],
       timeslots: ['', Validators.required],
       location: ['', Validators.required],
       message: ['', Validators.required],
+      request_sender: '',
+      request_receivers: '',
+      requested_by: '',
+      start_time: '',
+      end_time: '',
+      ent_id: '',
     });
   }
 
@@ -31,5 +39,17 @@ export class BookMeetingPage {
   bookMeetingFunc(val: any) {
     console.log(val.value);
     this.closeModal();
+    // try {
+    //   this.apiService.showLoader();
+    //   this.apiService.hitAPICall('post', 'event/send-meeting-request', val.value).subscribe((response: any) => {
+    //     this.apiService.hideLoader();
+    //   }, error => {
+    //     this.apiService.hideLoader();
+    //     this.apiService.showAlert('', 'Error form server side', 'Ok', () => { });
+    //   })
+    // } catch (error) {
+    //   this.apiService.hideLoader();
+    //   this.apiService.showAlert('', 'Error form server side', 'Ok', () => { });
+    // }
   }
-}
+} 

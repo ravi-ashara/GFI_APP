ab35ee66cc349f8e87d3533abc1ad964c5b81e14
ravi-ashara <raviashara94@gmail.com>
2020-04-04 15:37:08 +0530

    Display sponsors dynamic

--- src/app/Pages/sponsor/sponsor.page.ts
+++ src/app/Pages/sponsor/sponsor.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { ApiCallService } from '../../Services/api-call/api-call.service';
 
 @Component({
@@ -7,16 +7,22 @@ import { ApiCallService } from '../../Services/api-call/api-call.service';
   styleUrls: ['./sponsor.page.scss'],
 })
 export class SponsorPage {
-  currentSponsor: string = "Gold Sponsors";
-  totalSponsor = ['Gold Sponsors', 'Silver Sponsors', 'Bronze Sponsors', 'Financial Supporters', 'Governmental Supporters', 'Cooperations', 'Media Coverage']
-  constructor(public apiService: ApiCallService) { }
-
-  segmentChanged(val: any) {
-    console.log(val.detail.value);
-  }
+  currentSponsorIndex: number = 0;
+  sposorDataValue: any = []
+  totalSponsor: any = []
+  constructor(
+    public apiService: ApiCallService) { }
 
   ionViewWillEnter() {
     this.showSponsors();
+  }
+
+  selectedTab(index: any) {
+    this.sposorDataValue = this.totalSponsor.data[index];
+  }
+
+  openDetails(val: any) {
+    console.log(val);
   }
 
   showSponsors() {
@@ -24,7 +30,10 @@ export class SponsorPage {
       this.apiService.showLoader();
       this.apiService.hitAPICall('post', 'sponsor/list', '').subscribe((response: any) => {
         this.apiService.hideLoader();
-        console.log(response);
+        if (response.status === "success") {
+          this.totalSponsor = response;
+          this.sposorDataValue = response.data[this.currentSponsorIndex];
+        }
       }, error => {
         this.apiService.hideLoader();
         this.apiService.showAlert('', 'Error form server side', 'Ok', () => { });

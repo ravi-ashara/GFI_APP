654a338b7cbe603573177c7c7cd34211d389c4b6
ravi-ashara <raviashara94@gmail.com>
2020-04-25 00:10:59 +0530

    Submit book meeting form. #1

--- src/app/Pages/create-meeting/create-meeting.page.ts
+++ src/app/Pages/create-meeting/create-meeting.page.ts
@@ -3,7 +3,7 @@ import { ModalController, NavParams } from '@ionic/angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiCallService } from '../../Services/api-call/api-call.service';
 import { NetworkService, ConnectionStatus } from '../../Services/network/network.service';
-
+import * as moment from 'moment';
 @Component({
   selector: 'app-create-meeting',
   templateUrl: './create-meeting.page.html',
@@ -16,34 +16,46 @@ export class CreateMeetingPage {
   public createMeetingForm: FormGroup;
   public eventListByCompanyData: any = [];
   public userListByEvent: any = [];
+  public selectedParticipants: any = [];
+  public setMaxDate: any;
+  public setMinDate: any;
+  public selectedEventData: any;
+  public userFreeSlots: any;
   constructor(
     private modalCtrl: ModalController,
     public navParam: NavParams,
-    private formBuilder: FormBuilder,
+    public formBuilder: FormBuilder,
     public commonService: ApiCallService,
     private networkService: NetworkService) {
     this.modalData = this.navParam.get('value');
     this.loginUserDetails = JSON.parse(localStorage.loginUserData);
 
-    console.log(this.modalData);
-    console.log(this.loginUserDetails);
-
     this.createMeetingForm = this.formBuilder.group({
       event_id: ['', Validators.required],
       request_receivers: ['', Validators.required],
       meeting_date: ['', Validators.required],
-      timeslots: ['', Validators.required],
-      location: ['', Validators.required],
       start_time: ['', Validators.required],
       end_time: '',
       meeting_details: '',
-      request_sender: this.modalData,
-      requested_by: ''
+      request_sender: this.modalData.u_id,
+      requested_by: this.modalData.u_id,
+      ent_id:''
     });
   }
 
   ionViewWillEnter() {
     this.eventListByCompany();
+  }
+
+  addParticipants(val: any) {
+    let val_1 = val.target.textContent.trimStart();
+    let val_2 = val_1.split(" ");
+    for (let i = 0; i < val_2.length; i++) {
+      if (i % 2 == 0) {
+        this.selectedParticipants.push(val_2[i]);
+      }
+    }
+    this.selectedParticipants.toString();
   }
 
   eventListByCompany() {
@@ -76,7 +88,7 @@ export class CreateMeetingPage {
     if (this.networkService.getCurrentNetworkStatus() === ConnectionStatus.Online) {
       try {
         const passData = {
-          event_id: val,
+          event_id: val.target.value,
           organization_id: this.modalData.organization_id
         }
 
@@ -84,6 +96,10 @@ export class CreateMeetingPage {
         this.commonService.hitAPICall('post', 'event/get-user-list-by-event', passData).subscribe((response: any) => {
           this.commonService.hideLoader();
           this.userListByEvent = response;
+          const indx = this.eventListByCompanyData.events.findIndex(e => e.event_id == val.target.value);
+          this.selectedEventData = this.eventListByCompanyData.events[indx];
+          this.setMinDate = moment(this.selectedEventData.event_start_datetime).format('YYYY-MM-DD');
+          this.setMaxDate = moment(this.selectedEventData.event_end_datetime).format('YYYY-MM-DD');
         }, error => {
           this.commonService.serverSideError();
         });
@@ -92,6 +108,31 @@ export class CreateMeetingPage {
       }
     } else {
       this.commonService.showToastWithDuration('You are Offline', 'top', 3000);
+    }
+  }
+
+  getFreeSlote(val: any) {
+    if (this.networkService.getCurrentNetworkStatus() === ConnectionStatus.Online) {
+      try {
+        const passData = {
+          event_id: this.selectedEventData.event_id,
+          meeting_date: moment(val.target.value).format('YYYY-MM-DD'),
+          request_sender: this.modalData.u_id,
+          request_receivers: this.createMeetingForm.value.request_receivers
+        }
+
+        this.commonService.showLoader();
+        this.commonService.hitAPICall('post', 'event/get-free-slote', passData).subscribe((response: any) => {
+          this.commonService.hideLoader();
+          if (response.status === "success") {
+            this.userFreeSlots = response.data;
+          }
+        }, error => {
+          this.commonService.showToastWithDuration('You are Offline', 'top', 3000);
+        });
+      } catch (error) {
+        this.commonService.showToastWithDuration('You are Offline', 'top', 3000);
+      }
     }
   }
 
@@ -104,12 +145,15 @@ export class CreateMeetingPage {
   }
 
   bookMeeting(val: any) {
+    val.meeting_date = moment(val.meeting_date).format('YYYY-MM-DD');
+    let splitData: any = val.start_time.split("-");
+    val.start_time = splitData[0];
+    val.end_time = splitData[1];
     if (this.networkService.getCurrentNetworkStatus() === ConnectionStatus.Online) {
       try {
         this.commonService.showLoader();
-        this.commonService.hitAPICall('post', 'event/send-meeting-request', val.value).subscribe((response: any) => {
+        this.commonService.hitAPICall('post', 'event/send-meeting-request', val).subscribe((response: any) => {
           this.commonService.hideLoader();
-          console.log(response);
         }, error => {
           this.commonService.serverSideError();
         });

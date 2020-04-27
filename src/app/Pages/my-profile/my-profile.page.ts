69ab4593b8a5bac3852230e23604fd8a74a24d77
ravi-ashara <raviashara94@gmail.com>
2020-04-28 00:11:08 +0530

    Click on edit profile show form. #13 Added "Personal" and "Organization" tab for edit specific from Added 3 step Organization form.

--- src/app/Pages/my-profile/my-profile.page.ts
+++ src/app/Pages/my-profile/my-profile.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiCallService } from '../../Services/api-call/api-call.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-my-profile',
@@ -8,9 +9,71 @@ import { ApiCallService } from '../../Services/api-call/api-call.service';
 })
 export class MyProfilePage {
   userData: any = {};
-  constructor(public commonService: ApiCallService) { }
+  showEditProfileForm: boolean = false;
+  userImage: any = '';
+  segmentSelectedValue: string = 'Personal';
+  registerForm: FormGroup;
+  setStep: string = "step1";
+  buttonName: string = '';
+
+  constructor(public commonService: ApiCallService,
+    public formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      u_first_name: ['', Validators.required],
+      u_last_name: ['', Validators.required],
+      u_headline: '',
+      u_gender: ['', Validators.required],
+      u_about_us: '',
+      u_personal_website: '',
+      u_social_link: '',
+      u_email: ['', Validators.email],
+      profile_pic: '',
+    });
+  }
 
   ionViewWillEnter() {
     this.userData = this.commonService.getUserLoginData();
+  }
+
+  toggleEditProfile() {
+    this.showEditProfileForm = !this.showEditProfileForm;
+    this.segmentSelectedValue = 'Personal';
+    this.setStep = "step1";
+    this.buttonName = '';
+  }
+
+  openImage() {
+    this.commonService.openCamera('PHOTOLIBRARY', (imageData: any) => {
+      if (imageData != 'Error') {
+        this.userImage = imageData;
+      }
+    });
+  }
+
+  submitRegisterForm(val: any) {
+    console.log(val);
+    this.toggleEditProfile();
+  }
+
+  changeStep(val: any, val_buttonName: any) {
+    this.setStep = val;
+    this.buttonName = val_buttonName;
+  }
+
+  registerProfile() {
+    this.toggleEditProfile();
+  }
+
+  getPhotos(val: any) {
+    this.commonService.openCamera('PHOTOLIBRARY', (imageData: any) => {
+      if (imageData !== "Error") {
+        if (val == "Add company logo") {
+          console.log(imageData)
+        }
+        else {
+          console.log(imageData)
+        }
+      }
+    });
   }
 }

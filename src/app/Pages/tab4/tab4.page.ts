275cb5907aebc62929ce72539d81687ad42101a8
ravi-ashara <raviashara94@gmail.com>
2020-05-02 16:10:57 +0530

    Call API to get company data. #21 Show dynamic company data. Show default company placeholder if image not found

--- src/app/Pages/tab4/tab4.page.ts
+++ src/app/Pages/tab4/tab4.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiCallService } from '../../Services/api-call/api-call.service';
+import { NetworkService, ConnectionStatus } from '../../Services/network/network.service';
 
 @Component({
   selector: 'app-tab4',
@@ -8,6 +9,44 @@ import { ApiCallService } from '../../Services/api-call/api-call.service';
 })
 export class Tab4Page {
 
-  constructor(public apicall: ApiCallService) { }
+  companyData: any = [];
 
+  constructor(public commonService: ApiCallService,
+    private networkService: NetworkService) { }
+
+  ionViewWillEnter() {
+    this.showCompanies();
+  }
+
+  showCompanies() {
+    if (this.networkService.getCurrentNetworkStatus() === ConnectionStatus.Online) {
+      try {
+        this.commonService.showLoader();
+        this.commonService.hitAPICall('post', 'organization/list', '').subscribe((response: any) => {
+          this.commonService.hideLoader();
+          if (response.status == "success") {
+            this.companyData = response.data;
+          }
+        }, error => {
+          this.commonService.serverSideError();
+        });
+      } catch (error) {
+        this.commonService.serverSideError();
+      }
+    } else {
+      this.commonService.showToastWithDuration('You are Offline', 'top', 3000);
+    }
+  }
+
+  createnewList(val: any) {
+    console.log(val);
+  }
+
+  gotoChatList(val: any) {
+    console.log(val);
+  }
+
+  errorImage(val: any) {
+    return val.target.src = "assets/images/company-placeholder.jpg";
+  }
 }

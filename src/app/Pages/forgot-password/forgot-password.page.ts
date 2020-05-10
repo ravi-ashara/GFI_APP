d86d2c6bb555e3ced3259917eb868f6e3611a7be
ravi-ashara <raviashara94@gmail.com>
2020-05-10 13:05:56 +0530

    Setup API Call #11

--- src/app/Pages/forgot-password/forgot-password.page.ts
+++ src/app/Pages/forgot-password/forgot-password.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ApiCallService } from '../../Services/api-call/api-call.service';
 import { NetworkService, ConnectionStatus } from '../../Services/network/network.service';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -12,7 +13,8 @@ export class ForgotPasswordPage {
   forgotPasswordForm: any;
   constructor(public formBuilder: FormBuilder,
     public commonService: ApiCallService,
-    private networkService: NetworkService) {
+    private networkService: NetworkService,
+    private router: Router) {
     this.forgotPasswordForm = this.formBuilder.group({
       u_email: ['', Validators.email],
     });
@@ -21,7 +23,21 @@ export class ForgotPasswordPage {
   submitForm(val: any) {
     if (this.networkService.getCurrentNetworkStatus() === ConnectionStatus.Online) {
       try {
-
+        this.commonService.showLoader();
+        this.commonService.hitAPICall('post', 'forgotPassword', val.value).subscribe((response: any) => {
+          this.commonService.hideLoader();
+          console.log(response);
+          if (response.status == 'true') {
+            const navigationExtras: NavigationExtras = {
+              state: {
+                pageName: 'forgotPassword',
+              }
+            };
+            this.router.navigate(['set-password'], navigationExtras);
+          }
+        }, error => {
+          this.commonService.serverSideError();
+        });
       } catch (error) {
         this.commonService.serverSideError();
       }

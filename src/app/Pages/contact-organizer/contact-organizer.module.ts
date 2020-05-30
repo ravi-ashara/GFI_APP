9ea37904daa70a33036664a7f916ef14d4428e89
ravi-ashara <raviashara94@gmail.com>
2020-05-30 17:39:15 +0530

    Submit form. #9

--- src/app/Pages/contact-organizer/contact-organizer.module.ts
+++ src/app/Pages/contact-organizer/contact-organizer.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -15,7 +15,8 @@ import { ComponentsModule } from '../../Components/components.module';
     FormsModule,
     IonicModule,
     ContactOrganizerPageRoutingModule,
-    ComponentsModule
+    ComponentsModule,
+	ReactiveFormsModule
   ],
   declarations: [ContactOrganizerPage]
 })
--- src/app/Pages/contact-organizer/contact-organizer.page.ts
+++ src/app/Pages/contact-organizer/contact-organizer.page.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
 import { ApiCallService } from '../../Services/api-call/api-call.service';
-
+import { NetworkService, ConnectionStatus } from '../../Services/network/network.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-contact-organizer',
   templateUrl: './contact-organizer.page.html',
   styleUrls: ['./contact-organizer.page.scss'],
 })
-export class ContactOrganizerPage{
+export class ContactOrganizerPage {
+  public userData: any;
+  public contactForm: FormGroup;
+  constructor(public formBuilder: FormBuilder,
+    private commonService: ApiCallService,
+    private networkService: NetworkService) {
+    this.userData = this.commonService.getUserLoginData();
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.email],
+      phone: ['', Validators.required],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+  
+  ionViewWillEnter(){
+    this.contactForm.get('name').setValue(this.userData.u_first_name + ' ' + this.userData.u_last_name);
+    this.contactForm.get('email').setValue(this.userData.u_email);
+  }
 
-  constructor(public apicall: ApiCallService) { }
-
+  submitForm(val: any) {
+    if (this.networkService.getCurrentNetworkStatus() === ConnectionStatus.Online) {
+      try {
+        this.commonService.showLoader();
+        this.commonService.http.post(environment.baseURL + 'contact-us/save', val.value).subscribe((response: any) => {
+          this.commonService.hideLoader();
+        }, error => {
+          this.commonService.serverSideError();
+        });
+      } catch (error) {
+        this.commonService.serverSideError();
+      }
+    } else {
+      this.commonService.showToastWithDuration('You are Offline', 'top', 3000);
+    }
+  }
 }
